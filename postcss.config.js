c8eb49aa5310b7e4d2b7c03f59fcdb2e1f19e4f6
XinYun2020 <xinyunzhang2016@gmail.com>
2023-05-30 01:00:55 +1000

    fixed webpack issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    "google-fonts-plugin": {
-      fonts: [
-        {
-          family: "IBM Plex Mono",
-          variants: ["300", "400", "500", "600", "700"],
-        },
-      ],
-    },
     autoprefixer: {},
   },
 };
