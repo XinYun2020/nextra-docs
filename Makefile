e48c414e898bde518d5ebe54280e7c4419f644ff
XinYun2020 <xinyunzhang2016@gmail.com>
2023-05-29 22:08:37 +1000

    fixed typo for deployment

--- Makefile
+++ Makefile
@@ -2,5 +2,7 @@ install:
 	pnpm install
 run:
 	pnpm run dev
+build:
+	pnpm run build
 note: 
 	touch ./pages/zettelkasten-src/new-note.mdx
--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,14 @@
 type TSectionData = {
-    type: 'work' | 'project' | 'edu' | 'extra';
-    what: string;
-    when: string;
-    role?: string;
-    how?: string[];
-}
+  type: "work" | "project" | "edu" | "extra";
+  what: string;
+  when: string;
+  role?: string;
+  how?: string[];
+};
 
 export const WORK_EXPERIENCES: TSectionData[] = [
   {
-    type: 'work'
+    type: "work",
     what: "Frontend Developer, IPIC Pty Ltd, George & Matilda",
     when: "Aug 2022 - Current",
     role: "",
@@ -25,20 +25,19 @@ export const WORK_EXPERIENCES: TSectionData[] = [
 ];
 
 export const PROJECTS: TSectionData[] = [
-    {
-      type: 'project'
-      what: "Frontend Developer, IPIC Pty Ltd, George & Matilda",
-      when: "Aug 2022 - Current",
-      role: "Frontend Engineer and UI/UX Designer",
-      how: [
-        `Responsible for development and maintenance of web applications using modern technologies like Typescript, ReactJs, Tailwind`,
-        `Built company styled design system.`,
-        `Frontend architecture`,
-        `Developing microservices`,
-        `Engaged in customer support for the existing code, troubleshoot and optimize the system.`,
-        `Produced planning, development, and deployment technical documentation.`,
-        `Present on tech decisions and component architecture designs.`,
-      ],
-    },
-  ];
-
+  {
+    type: "project",
+    what: "Frontend Developer, IPIC Pty Ltd, George & Matilda",
+    when: "Aug 2022 - Current",
+    role: "Frontend Engineer and UI/UX Designer",
+    how: [
+      `Responsible for development and maintenance of web applications using modern technologies like Typescript, ReactJs, Tailwind`,
+      `Built company styled design system.`,
+      `Frontend architecture`,
+      `Developing microservices`,
+      `Engaged in customer support for the existing code, troubleshoot and optimize the system.`,
+      `Produced planning, development, and deployment technical documentation.`,
+      `Present on tech decisions and component architecture designs.`,
+    ],
+  },
+];
