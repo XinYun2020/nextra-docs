4d13e1697b8b4eafbda18d812a6b4d85cb455522
XinYun2020 <xinyunzhang2016@gmail.com>
2023-05-29 15:22:55 +1000

    resturcture repo

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,12 +3,13 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>XinYun2020 Zettelkasten</span>,
-  // project: {
-  //   link: "https://github.com/shuding/nextra-docs-template",
-  // },
+  project: {
+    link: "https://github.com/XinYun2020",
+  },
   // chat: {
   //   link: "https://discord.com",
   // },
+  faviconGlyph: "✦",
   docsRepositoryBase: "https://github.com/XinYun2020/nextra-docs",
   footer: {
     text: "Copyright @XinYun2020",
