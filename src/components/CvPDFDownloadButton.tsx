1a55b412744e1d189d7cfbe387fc4499fac379c0
XinYun2020 <xinyunzhang2016@gmail.com>
2023-05-30 03:01:54 +1000

    updated cv

--- src/components/CvPDFDownloadButton.tsx
+++ src/components/CvPDFDownloadButton.tsx
@@ -3,15 +3,17 @@ import React from "react";
 const CvPDFDownloadButton: React.FC = () => {
   const handleDownload = () => {
     const downloadLink = document.createElement("a");
-    downloadLink.href = "/Xinyun_Zhang_CV.pdf";
+    downloadLink.href = "/Cv – Nextra.pdf";
     downloadLink.download = "Xinyun_Zhang_CV.pdf";
     downloadLink.click();
   };
 
   return (
-    <button onClick={handleDownload} className="btn">
-      Download CV
-    </button>
+    <div className="flex flex-col fixed bottom-0 left-0 right-0 opacity-60 hover:opacity-100 active:scale-75 duration-200">
+      <button onClick={handleDownload} className="btn">
+        Download CV
+      </button>
+    </div>
   );
 };
 
