c100fe491836c097daa6fa2bca15518aa6df3a50
XinYun2020 <xinyunzhang2016@gmail.com>
2023-05-30 03:35:21 +1000

    config DocsThemeConfig

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,19 +1,45 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
+// node_modules/nextra-theme-docs/dist/index.tsx
 const config: DocsThemeConfig = {
   logo: <span>XinYun2020 Zettelkasten</span>,
   project: {
     link: "https://github.com/XinYun2020",
   },
+  banner: {
+    dismissible: true,
+    key: "nextra-banner",
+  },
   // chat: {
   //   link: "https://discord.com",
   // },
+  darkMode: true,
+  direction: "ltr",
   faviconGlyph: "✦",
   docsRepositoryBase: "https://github.com/XinYun2020/nextra-docs",
-  footer: {
-    text: "Copyright @XinYun2020",
+  editLink: {
+    text: " ",
   },
+  feedback: {
+    // content: "Question? Provide feedback \u2192",
+    content: " ",
+    labels: "feedback",
+  },
+  footer: {
+    text: `Copyright ${
+      /* @__PURE__ */ new Date().getFullYear()
+    } \xA9 XinYun2020.`,
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: false,
+  },
+  toc: {
+    float: true,
+    title: "Page Contents",
+  },
+  useNextSeoProps: () => ({ titleTemplate: "%s \u2013 XinYun2020" }),
 };
 
 export default config;
