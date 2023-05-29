12bce77dee12b9b6b6f24e8379fb4a6575dc72bb
XinYun2020 <xinyunzhang2016@gmail.com>
2023-05-30 00:03:41 +1000

    added ibm plex mono

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
+    "google-fonts-plugin": {
+      fonts: [
+        {
+          family: "IBM Plex Mono",
+          variants: ["300", "400", "500", "600", "700"],
+        },
+      ],
+    },
     autoprefixer: {},
   },
-}
+};
