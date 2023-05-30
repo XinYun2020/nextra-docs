983856515f74cef9235fa286a3e500c24da004ed
XinYun2020 <xinyunzhang2016@gmail.com>
2023-05-30 14:44:12 +1000

    added react-confetti

--- src/components/CvPDFDownloadButton.tsx
+++ src/components/CvPDFDownloadButton.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Confetti from "react-confetti";
 
 const CvPDFDownloadButton: React.FC = () => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const [pieces, setPieces] = useState<number>(0);
+
+  const stopConfetti = () => {
+    setTimeout(() => {
+      setPieces(0);
+    }, 3000);
+  };
+
   const handleDownload = () => {
+    setIsClicked(true);
+    setPieces(200);
+
     const downloadLink = document.createElement("a");
     // downloadLink.href = "/Cv – Xinyun Zhang.pdf";
     downloadLink.href = "/Cv – Xinyun Zhang.pdf";
@@ -9,14 +22,18 @@ const CvPDFDownloadButton: React.FC = () => {
       /* @__PURE__ */ new Date().getFullYear()
     }.pdf`;
     downloadLink.click();
+    stopConfetti();
   };
 
   return (
-    <div className="flex flex-col fixed bottom-0 left-0 right-0 opacity-60 hover:opacity-100 active:scale-75 duration-200">
-      <button onClick={handleDownload} className="btn">
-        Download CV
-      </button>
-    </div>
+    <>
+      {isClicked && <Confetti gravity={0.2} numberOfPieces={pieces} />}
+      <div className="flex flex-col fixed bottom-0 left-0 right-0 opacity-60 hover:opacity-100 active:scale-75 duration-200">
+        <button onClick={handleDownload} className="btn">
+          Download CV
+        </button>
+      </div>
+    </>
   );
 };
 
