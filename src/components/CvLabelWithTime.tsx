c207435a9bafc1fa7ce40ce9a6649d0f2263c765
XinYun2020 <xinyunzhang2016@gmail.com>
2023-05-31 00:11:37 +1000

    update cv

--- src/components/CvLabelWithTime.tsx
+++ src/components/CvLabelWithTime.tsx
@@ -2,18 +2,18 @@ import React from "react";
 
 type CvLabelWithTimeProps = {
   label: string | React.ReactNode;
-  time: string;
+  time: string | React.ReactNode;
   highlight?: boolean;
 };
 
 const CvLabelWithTime = ({ label, time, highlight = true }) => {
   return (
     <div
-      className={`grid grid-cols-1 md:grid-cols-5 font-bold justify-between pt-2 ${
+      className={`flex flex-col md:flex-row font-bold justify-between pt-2 ${
         highlight && " text-teal-600 dark:text-teal-400"
       }`}>
-      <span className="text-lg md:col-span-4">{label}</span>
-      <span className="text-right md:col-span-1">{time}</span>
+      <span className="text-lg ">{label}</span>
+      <span className="text-right">{time}</span>
     </div>
   );
 };
