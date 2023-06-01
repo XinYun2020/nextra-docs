3fc7f5273a1f64b31b3eb594a49a11f21fb57cca
XinYun2020 <xinyunzhang2016@gmail.com>
2023-06-02 02:38:16 +1000

    add balancer and remove paramthesis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Provider } from "react-wrap-balancer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>test</title>
         <link
           href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css"
           rel="stylesheet"
         />
-      </Head>
-      <Component {...pageProps} />
+      </Head> */}
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
     </>
   );
 }
