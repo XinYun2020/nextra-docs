b8e95419c57386400d34008d7dfb11d40ebb8d36
XinYun2020 <xinyunzhang2016@gmail.com>
2023-07-28 14:14:11 +1000

    fixed compile issue

--- src/components/DocslyClient.tsx
+++ src/components/DocslyClient.tsx
@@ -5,7 +5,8 @@ import "@docsly/react/styles.css";
 import { usePathname } from "next/navigation";
 
 export default function DocslyClient() {
-  const pathname = usePathname();
+  const pathname = usePathname() || ""; // Use an empty string as the default value
+
   const publicId =
     "public_vQKVC7ryvcQ38AJpc4yeLLkc7vXQ1URK33Zo1tudBjf54DSVDJTKULfOGXLXpZQ0";
   return <Docsly publicId={publicId} pathname={pathname} />;
